1a35852f61c21db417ce752bf814f18994b31b32
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-06-13 12:24:04 +0530

    no message

--- my-crud-site/backend/index.js
+++ my-crud-site/backend/index.js
@@ -15,11 +15,13 @@ app.use("/api", userRoute);
 //connect to db
 connectDB();
 
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
-});
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
